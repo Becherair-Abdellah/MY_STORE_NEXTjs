73f17752b5993001c35014481fe683f11f46eb53
Becherair-Abdellah <abdeallahdz2004@gmail.com>
2023-09-04 17:11:53 +0100

    solve problem of backend

--- frontend/app/(auth)/signup/page.jsx
+++ frontend/app/(auth)/signup/page.jsx
@@ -1,5 +1,9 @@
 
+"use client"
+import { rigister } from "@/api/rigister"
+import { useState } from "react";
 export default function SignUp() {
+  const [dataFile,setFile] = useState('')
     return (
       <div className="h-full flex justify-center items-center">
         <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
@@ -34,7 +38,7 @@ export default function SignUp() {
 <span className="text-center mt-4 text-gray-500">or</span>
 
           <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-            <form className="space-y-6" action="#" method="POST">
+            <form className="space-y-6" action="https://www.google.com" method="POST">
               <div>
                 <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">
                   Email address
@@ -51,8 +55,16 @@ export default function SignUp() {
                 </div>
               </div>
               <div>
+                <input type="file" onChange={(e)=>{
+                  setFile(e.target.files[0]);
+                }}
+                accept="image/*" />
                 <button
                   type="submit"
+                  onClick={(e)=>{
+                    e.preventDefault();
+                    rigister(dataFile)
+                  }}
                   className="flex w-full justify-center rounded-md bg-gray-800 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-gray-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Sign in
--- frontend/app/(root)/page.js
+++ frontend/app/(root)/page.js
@@ -1,8 +1,16 @@
+'use client'
 import Image from 'next/image'
-
+import { useEffect } from 'react'
 export default function Home() {
   return (
     <>
+    <Image
+        src="/signups.png" // The path to your image in the public directory
+        alt="My Image"
+        width={100}          // The desired width of the image
+        height={1000} 
+        objectFit='cover'        // The desired height of the image
+      />
     Get Started Work Uisng Next Js
     </>
   )
